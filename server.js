d64ab1d7ee60c4d268d2708b17d462a38e8215d9
alokkavilkar <alokkavilkar1057@gmail.com>
2024-06-12 18:11:47 +0530

    dev/ api endpoint add

--- server.js
+++ server.js
@@ -14,7 +14,26 @@ app.get('/profile-picture', (req, res)=>{
     let img = fs.readFileSync(path.join(__dirname, "images/profile.png"));
     res.writeHead(200, { 'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
-})
+});
+
+// Fetch profile data
+app.get('/profile', (req, res) => {
+  profileCollection.findOne({ _id: 1 })
+      .then(profile => res.json(profile))
+      .catch(error => res.status(500).send(error));
+});
+
+// Update profile data
+app.post('/profile', (req, res) => {
+  const { name, email, interests } = req.body;
+  profileCollection.updateOne(
+      { _id: 1 },
+      { $set: { name, email, interests } },
+      { upsert: true }
+  )
+      .then(result => res.json('Profile updated'))
+      .catch(error => res.status(500).send(error));
+});
 
 // Start the server
 app.listen(port, () => {
